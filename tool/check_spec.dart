df90d49c9f8b0f2eb8aa985581448124cc7cf7a3
imoyan <suihei.hack@gmail.com>
2026-05-18 19:51:41 +0900

    Current blocked bundle の token flow server refを更新
    houra-server #397/#398 の merge 結果を current blocked bundle に反映しました。
    
    - server ref: dcec476b7f5157832ee96c24a18d3c46b2d5f5ef
    - server evidence source: imoyan/houra-server#397 / #398
    - Matrix v1.18 support claim と versions advertisement は広げていません。
    
    Verification:
    - dart tool/check_spec.dart
    - git diff --check

--- tool/check_spec.dart
+++ tool/check_spec.dart
@@ -11928,7 +11928,7 @@ void checkMatrixV118ReleaseEvidenceCurrentBlockedBundle(
       refs['release_candidate'] !=
           'houra-matrix-v1.18-current-blocked-2026-05-18' ||
       refs['houra_spec_ref'] != 'fd05905d39b8cb64fbcabe4d2a36d9a62855c3ac' ||
-      refs['houra_server_ref'] != 'bdfd2fc7ef33ab3783cfa0866d75ea5eec5e62eb' ||
+      refs['houra_server_ref'] != 'dcec476b7f5157832ee96c24a18d3c46b2d5f5ef' ||
       refs['houra_client_ref'] != '5154a2c519d113f030dc0e9369d06060005ebd49') {
     failures.add('${relative(file)} current candidate refs invalid.');
   }
@@ -11938,8 +11938,8 @@ void checkMatrixV118ReleaseEvidenceCurrentBlockedBundle(
   final client = evidenceSources is Map ? evidenceSources['client'] : null;
   if (server is! Map ||
       server['repo'] != 'houra-server' ||
-      server['issue'] != 'imoyan/houra-server#395' ||
-      server['pull_request'] != 'imoyan/houra-server#396' ||
+      server['issue'] != 'imoyan/houra-server#397' ||
+      server['pull_request'] != 'imoyan/houra-server#398' ||
       server['merge_commit'] !=
           (refs is Map ? refs['houra_server_ref'] : null) ||
       server['head_under_test'] !=
